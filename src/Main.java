a09368e050c1ac37b20e70fd5c40da76bcd5d73c
dinmuhamedbolat-ctrl <dinmuhamedbolat@gmail.com>
2026-01-18 20:40:25 +0500

    Initial commit5

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,53 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        MenuItem beshbarmak = new MenuItem("Бешбармак", 6);
-        MenuItem quirdaq = new MenuItem("Куырдак", 9);
-        MenuItem sprite = new MenuItem("Спрайт 0,5", 2);
-        MenuItem greeceSalad = new MenuItem("Греческий салат", 5);
-        MenuItem moroccoTea = new MenuItem("Марроканский чай", 3);
-        MenuItem bread = new MenuItem("Лепешка свежая", 1);
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "0000";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
 
-        Order order1 = new Order(1);
-        order1.addItem(beshbarmak);
-        order1.addItem(greeceSalad);
-        order1.addItem(sprite);
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-        Order order2 = new Order(2);
-        order2.addItem(quirdaq);
-        order2.addItem(bread);
-        order2.addItem(moroccoTea);
+        int totalSum = 0;
+        String orderDetails = "";
 
-        Product[] products = new Product[] {
-                beshbarmak, quirdaq, greeceSalad, sprite,
-                order1, order2
-        };
+        while (true) {
+            System.out.println("\n--- МЕНЮ ---");
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM restaraunt ORDER BY id ASC");
 
-        for (Product product : products) {
-            System.out.println(product);
+            while (result.next()) {
+                System.out.println(result.getInt("id") + ". " + result.getString("name") + " — " + result.getInt("price") + "$");
+            }
+
+            System.out.println("\nВведите номер блюда чтобы добавть в ваш заказ (или 0 для завершения заказа):");
+            int choice = scanner.nextInt();
+
+            if (choice == 0) {
+                System.out.println("\nВаш чек");
+                System.out.println( (orderDetails.isEmpty() ? " ": orderDetails));
+                System.out.println("Общая сумма  " + totalSum + "$");
+                connection.close();
+                break;
+            } else {
+                PreparedStatement ps = connection.prepareStatement("SELECT name, price FROM restaraunt WHERE id = ?");
+                ps.setInt(1, choice);
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    String name = rs.getString("name");
+                    int price = rs.getInt("price");
+
+                    totalSum += price;
+                    orderDetails += name + " ";
+
+                    System.out.println(name + " добавлен. Текущая сумма: " + totalSum + "$");
+                } else {
+                    System.err.println("такого блюда " + choice + " не существует.");
+                }
+            }
         }
-
-        System.out.println("Заказы одиннаковы?:" +
-                order1.equals(order2));
     }
 }
